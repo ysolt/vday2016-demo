5cb2a55ef1f825e17bae84b6d5168b6d5497dbbf
Zsolt Molnar <ysolt@ysolt.net>
2016-11-24 10:11:14 +0100

    add scripts to initialise data storage

--- scripts/container_run.sh
+++ scripts/container_run.sh
@@ -13,14 +13,17 @@ if [[ $ENVIRONMENT == 'aws' ]]; then
   docker pull ${IMAGE}
   docker tag ${IMAGE} drupal:${VERSION}
   docker logout "https://${IMAGE_REPO}"
+  mkdir -p /vagrant /data
+  yum install -y wget
+  wget https://s3.eu-central-1.amazonaws.com/vday2016-demo/data.tgz -O /vagrant/data.tgz
 else 
   docker pull 10.0.2.2:5000/${IMAGE_NAME}:${VERSION}
   docker tag 10.0.2.2:5000/${IMAGE_NAME}:${VERSION} ${IMAGE_NAME}:${VERSION}
 fi
 
-mkdir -p /data/mysql /data/drupal
+mkdir -p /data/
+tar xfz /vagrant/data.tgz -C /data
 chown -R 27.27 /data/mysql
 chown -R 997.997 /data/drupal
-#docker run --restart=always -v /data/mysql:/var/lib/mysql -v /data/drupal:/var/www/drupal/sites -d --name drupal-app --privileged -p 80:80 ${IMAGE_NAME}:${VERSION}
 
-docker run --restart=always -v /data/mysql:/var/lib/mysql -d --name drupal-app --privileged -p 80:80 ${IMAGE_NAME}:${VERSION}
+docker run --restart=always -v /data/mysql:/var/lib/mysql -v /data/drupal:/var/www/drupal/sites -d --name drupal-app --privileged -p 80:80 ${IMAGE_NAME}:${VERSION}
